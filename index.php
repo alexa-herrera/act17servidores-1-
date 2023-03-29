1103a8c18e9f282f52dd0aa0212c1b77d63764a3
alexaherrera03 <>
2023-03-29 13:56:51 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>act17</title>
+  </head>
+  <body>
+    <?php
+ 
+    echo'<p>Nombre:Alexandra Yamileth Covarrubias Herrera</p>'
+?>
+   <?php
+     echo '<p>Calle:Constitucion</p>'
+?>
+    <?php
+      echo '<p>Colonia: Magisterio</p>'
+?>
+   <?php
+    echo'<p>Municipio: Ramos Arizpe</p>'
+?>
+   <?php
+    echo '<p>Telefono: 7429305840</p>'
+?>
+   <?php
+    echo '<p>Num de casa: 931'
+      ?>
+   
+  </body>
+</html>
---
+++ read.md
@@ -0,0 +1,12 @@
+1.Inicio
+2.Leer Nombre
+3.Mostrar 'Alexandra Yamileth Covarrubias Herrera'
+4.Leer Calle
+5.Mostrar 'Constitucion'
+6.Leer Municipio
+7.Mostrar 'Ramos Arizpe'
+8.Leer Telefono
+9.Mostrar '7429305840'
+10.Leer Num de casa
+11.Mostrar '931'
+12.Fin
